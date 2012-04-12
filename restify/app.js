5586bdc4c3f23fa845646eaece0072ef8850b1c1
Stéphane Gully <stephane.gully@gmail.com>
2012-04-13 00:05:16 +0200

    ajout de restify

---
+++ restify/app.js
@@ -0,0 +1,38 @@
+var restify = require('restify');
+
+var server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+//server.use(restify.queryParser());
+//server.use(restify.bodyParser());
+
+server.get('/', function (req, res, next) {
+  var nb_chunk = 0;
+  
+  req.setEncoding('utf-8');
+  req.on('data', function(chunk) {
+    nb_chunk++;
+    res.write('chunk received l='+chunk.length+'\n');
+    
+    if (nb_chunk < 5) {
+        // 1 chunk per second
+        req.pause();
+        setTimeout(function(){
+            req.resume();
+        }, 1000);
+    } else {
+        res.end(); // on stoppe le streaming au bout de 5 chunk
+    }
+  });
+  req.on('end', function() {
+    res.end();
+  });
+  
+  return next();
+});
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
