70d840cee7f2e89080e11e06f8cebf953e8f7242
Stéphane Gully <stephane.gully@gmail.com>
2012-04-12 23:36:15 +0200

    finalisation du test pour express

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,7 +1,23 @@
 
 exports.index = function(req, res){
-  var BufferStream = require('bufferstream')
-  var s = new BufferStream(/*{encoding:'utf8', size:'flexible'}*/)
-  s.pipe(res);
-  req.pipe(s);
+  var nb_chunk = 0;
+  
+  req.setEncoding('utf-8');
+  req.on('data', function(chunk) {
+    nb_chunk++;
+    res.write('chunk received l='+chunk.length+'\n');
+    
+    if (nb_chunk < 5) {
+        // 1 chunk per second
+        req.pause();
+        setTimeout(function(){
+            req.resume();
+        }, 1000);
+    } else {
+        res.end(); // on stoppe le streaming au bout de 5 chunk
+    }
+  });
+  req.on('end', function() {
+    res.end();
+  });
 };
